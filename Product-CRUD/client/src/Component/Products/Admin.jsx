d13da023ea2f6586b3219fb5be205fe77aa264a6
vasudev290 <vasukesavulu@gmail.com>
2024-07-27 16:08:59 +0530

    fix: Added delete method

--- Product-CRUD/client/src/Component/Products/Admin.jsx
+++ Product-CRUD/client/src/Component/Products/Admin.jsx
@@ -1,10 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import  Axios  from 'axios'
 let Admin= ()=> {
+
+  let [product, setProducts]=useState([])
+
+    useEffect(()=>{
+      getProducts();
+    },[])
+
+let getProducts =()=>{
+  Axios.get('http://127.0.0.1:5000/api/products')
+      .then((resp)=>{
+      setProducts(resp.data)
+      })
+      .catch(()=>{})
+}      
+let deleteProduct= (p_Id)=>{
+  /*
+    USAGE : Delete a Product
+    URL : http://127.0.0.1:5000/api/products/:id
+    Method : DELETE
+    Fields : no-fields
+ */
+let url= `http://127.0.0.1:5000/api/products/${p_Id}`
+Axios.delete(url)
+.then((resp)=>{
+    getProducts()
+})
+.catch((err)=>{})
+}
   return (
     <div>
-        <Link to="/create" className='btn btn-success'>Create Products</Link>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-8">
+          <Link to="/create" className='btn btn-success'>Create Products</Link>
+          </div>
+        </div>
+        <div className="row">
+          <pre>{JSON.stringify(product)}</pre>
+          <div className="col">
+            <table className='table'>
+              <thead>
+                <tr>
+                  <th>S.no</th>
+                  <th>Name</th>
+                  <th>Product</th>
+                  <th>Price</th>
+                  <th>Qty</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                  {
+                    product.length > 0 ? <>
+                    {
+
+                      product.map((product,index)=>{
+                        return<tr>
+                          <td>{index+1}</td>
+                          <td>{product.name}</td>
+                          <td><img src={product.image} height={"75px"} alt="" /></td>
+                          <td>{product.price}</td>
+                          <td>{product.qty}</td>
+                          <td>
+                            <Link to="/update-product/" className='btn btn-success'>UPDATE</Link>
+                            <button onClick={deleteProduct.bind(null,product._id)} className='btn btn-danger ml-2'>DELETE</button>
+                          </td>
+                        </tr>
+                      })
+                    }
+                    </>:<><h3>No Data</h3></>
+                  }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
