65fc904b737b74bdf07e8740d9161e8432f261bc
vasudev290 <vasukesavulu@gmail.com>
2024-08-02 18:25:36 +0530

    Feat: Created new Practice2.0 for another react practice

--- react-31/src/Redux/Store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-//import { useReducer } from "react";
-import {composeWithDevTools} from '@redux-devtools/extension'
-import {thunk} from 'redux-thunk'
-import {logger} from 'redux-logger'
-import { userReduser } from "./User/user.reducer";
-let store= createStore(userReduser, composeWithDevTools(applyMiddleware(thunk, logger)))
-
-export {store}
