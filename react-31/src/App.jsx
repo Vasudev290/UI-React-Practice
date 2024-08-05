c8f73fbcc008f55ffb50605a0000bf1cb7001830
vasudev290 <vasukesavulu@gmail.com>
2024-08-05 18:42:14 +0530

    feat:learning Function component

--- react-31/src/App.jsx
+++ react-31/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import Header from './Compnent/Header'
+import Header from './Comoponent/Header'
+import Slider from './Comoponent/Slider'
+import Footer from './Comoponent/Footer'
+import Movies from './Movies'
 let App = () => {
   return (
     <div>
       <h1>App Component</h1>
       <hr />
-      <Header />
+      < Movies />
     </div>
   )
 }
---
+++ react-31/src/Comoponent/Slider.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Slider extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Slider Comoponent</h2>
+      </div>
+    )
+  }
+}
+
+export default Slider
