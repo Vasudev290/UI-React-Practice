6c82035ecd4a0633ae1ccc0c72ada13a41d8eee5
vasudev290 <vasukesavulu@gmail.com>
2024-08-07 18:57:05 +0530

    Feat:Added new files

---
+++ Practice2.0/React -3/src/Comoponent/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Emp from './Emp'
+const User = () => {
+  return (
+    <div>
+        <h2>User Component</h2>
+        <Emp id={3467} Name={"Arjun"}/>
+    </div>
+  )
+}
+
+export default User
