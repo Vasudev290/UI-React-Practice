c86cb40033856bbee7d97f173749e955929a0401
Vasudev290 <vasukesavulu@gmail.com>
2024-12-09 18:40:41 +0530

    props

--- YouTube/Kalyan/react-31/src/Components/One.jsx
+++ YouTube/Kalyan/react-31/src/Components/One.jsx
@@ -12,6 +12,7 @@ const One = () => {
         <div className="container">
             <h2>{a}</h2>
             <h2>{b}</h2>
+            <img src="https://www.webbikeworld.com/wp-content/uploads/2023/03/bullet-350-specifications.jpg" alt="" width="750"/>
             <button className='btn btn-success'>Submit</button>
         </div>
     </div>
---
+++ YouTube/Kalyan/react-31/src/PropsContainer/SecondComp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SecondComp = (props) => {
+  return (
+    <div>
+        <h2>Component B</h2>
+        <h4>Component two: {props.name}</h4>
+    </div>
+  )
+}
+
+export default SecondComp
