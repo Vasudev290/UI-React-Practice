da7506de973f8fbe0b6db3e9f3df2d67cf160020
Vasudev290 <vasukesavulu@gmail.com>
2024-12-09 17:52:11 +0530

    style

--- YouTube/Kalyan/react-31/src/App.jsx
+++ YouTube/Kalyan/react-31/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import One from './Components/One'
 const App = () => {
   return (
     <div>
       <h2>App Component</h2>
+      <One />
     </div>
   )
 }
