3a373600dde861d5f29b36d48051a882191d19ec
Vasudev290 <vasukesavulu@gmail.com>
2024-12-09 21:57:22 +0530

    hooks

--- YouTube/Kalyan/react-31/src/App.jsx
+++ YouTube/Kalyan/react-31/src/App.jsx
@@ -1,21 +1,12 @@
 import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import One from './Components/One'
-import FirstComp from './PropsContainer/FirstComp'
-import SecondComp from './PropsContainer/SecondComp'
-import ThirdComp from './PropsContainer/ThirdComp'
-import FourthComp from './PropsContainer/FourthComp'
-import CompA from './propsDestructure/CompA'
+import One from './UseState/One'
+
 const App = () => {
   return (
     <div>
       <h2>App Component</h2>
-      <One />
-      <FirstComp name= "Chenna Kesavulu"/>
-      <SecondComp name= "Mahalakshmi "/>
-      <ThirdComp name= "Vasu Dev"/>
-      <FourthComp name= "Sushmitha"/>
-      <CompA/>
+      <One/>
     </div>
   )
 }
