e8c214b4a66884a3350a5a97ec4e7fc444d1fe2c
Vasudev290 <vasukesavulu@gmail.com>
2024-12-12 19:18:39 +0530

    ReactRouter

---
+++ YouTube/Kalyan/ReactPractice/ReactRouter -12/src/Components/Service.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Service = () => {
+  return (
+    <div>
+        <h2>Service Page</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Et a libero expedita voluptates dolorum eligendi qui odio, 
+            natus obcaecati quo saepe. Voluptate nesciunt ipsam ea 
+            harum sequi consequuntur eum! Nobis expedita amet rem 
+            quidem perspiciatis numquam, ipsa blanditiis adipisci 
+            distinctio, a porro maiores itaque sed ut iste incidunt 
+            quod aut?</p>
+    </div>
+  )
+}
+
+export default Service
