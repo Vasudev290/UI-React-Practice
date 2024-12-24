0ca54206795280c28b720d7d21a2d317b5c409cc
Vasudev290 <vasukesavulu@gmail.com>
2024-12-24 10:11:42 +0530

    redux Setup

---
+++ YouTube/Kalyan/newreact-31/src/Message/Message.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div>
+        <h3>Message Component</h3>
+        <hr />
+        <h3>Value : Hello Gm</h3>
+        <button>GM</button>
+        <button>GN</button>
+    </div>
+  )
+}
+
+export default Message
