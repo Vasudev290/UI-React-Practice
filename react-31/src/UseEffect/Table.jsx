406448ede9737d18a69ffd6bf62f49881c2fef29
Vasudev290 <vasukesavulu@gmail.com>
2025-01-04 18:16:14 +0530

    with async&await

--- react-31/src/UseEffect/Table.jsx
+++ react-31/src/UseEffect/Table.jsx
@@ -3,11 +3,32 @@ import React, { useEffect, useState } from 'react'
 
 const Table = () => {
     const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    const fetchData = async () => {
+        try{
+            const response = await fetch("https://fakestoreapi.com/products")
+            const result = await response.json();
+            setData(result)
+        } catch(err) {
+            if (err) throw err
+        } finally{
+            setLoading(false)
+        }
+    }
     useEffect(() => {
-        fetch("https://fakestoreapi.com/products")
-        .then((res) => res.json())
-        .then((json) => setData(json))
+        fetchData()
     }, [])
+
+    if(loading) {
+        return (
+            <div className="container text-center mt-5">
+                <div className="spinner-border text-primary" role='status'>
+                    <span className='sr-only'>Loading..!</span>
+                </div>
+                <p>Loading Products...</p>
+            </div>
+        )
+    }
   return (
     <div className='container mt-5'>
         <h2 className='mb-4'>Product Cards</h2>
