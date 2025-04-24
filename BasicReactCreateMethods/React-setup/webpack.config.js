aaf522d3b714556b984fbf195cf7a67f55e650bc
Vasudev290 <vasukesavulu@gmail.com>
2025-04-24 18:09:32 +0530

    install

--- BasicReactCreateMethods/React-setup/webpack.config.js
+++ BasicReactCreateMethods/React-setup/webpack.config.js
@@ -1,8 +1,23 @@
 const path = require("path");
-module.export = {
+
+module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,         // or /\.jsx?$/
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
   },
 };
